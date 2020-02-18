8db535b535582ade4ea868a9b69d074fe363ef2d
Felipe Lopes <lipe.pupo@hotmail.com>
2020-02-18 01:47:47 +0000

    Ai droga

---
+++ laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function verUsuario()
+    {
+        echo"<h1>Controlede Usúario</h1>";
+    }
+}
